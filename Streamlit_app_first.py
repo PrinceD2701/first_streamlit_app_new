916a2b4e3617672fa269a8199e413ce05d74d364
PrinceD2701 <148716300+PrinceD2701@users.noreply.github.com>
2023-11-28 00:25:34 +0530

    solved error

--- Streamlit_app_first.py
+++ Streamlit_app_first.py
@@ -12,4 +12,4 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 
-fruit_list = pandas.fruit_list('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruit_list = pandas.fruit_list("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
